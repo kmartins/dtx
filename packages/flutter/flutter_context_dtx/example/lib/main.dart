12504f6062674349682e6ff9acc9539a30452c40
Kauê Martins <ksampaiosp@gmail.com>
2022-08-17 10:25:01 -0300

    ci: remove print

--- packages/flutter/flutter_context_dtx/example/lib/main.dart
+++ packages/flutter/flutter_context_dtx/example/lib/main.dart
@@ -39,43 +39,32 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    print('-----------------------');
-    print('Is Phone ${context.isPhoneScreen}');
-    print('Is tablet ${context.isTabletScreen}');
-    print('Is Laptop ${context.isLaptopScreen}');
-    print('Is Desktop ${context.isDesktopScreen}');
     return Scaffold(
       backgroundColor: context.scaffoldBackgroundColor,
       appBar: AppBar(),
       body: Center(
-        child: Container(
-          width: context.widthBody,
-          color: Colors.red,
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: () => context.pushNamed(Routes.form),
+                child: Text(
+                  'Form Example',
+                  style: context.headline6TextStyle,
+                ),
+              ),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () => context.pushNamed(Routes.focusScope),
+                child: Text(
+                  'Focus Scope Example',
+                  style: context.headline6TextStyle,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      // body: Center(
-      //   child: SingleChildScrollView(
-      //     child: Column(
-      //       children: <Widget>[
-      //         ElevatedButton(
-      //           onPressed: () => context.pushNamed(Routes.form),
-      //           child: Text(
-      //             'Form Example',
-      //             style: context.headline6TextStyle,
-      //           ),
-      //         ),
-      //         const SizedBox(height: 16),
-      //         ElevatedButton(
-      //           onPressed: () => context.pushNamed(Routes.focusScope),
-      //           child: Text(
-      //             'Focus Scope Example',
-      //             style: context.headline6TextStyle,
-      //           ),
-      //         ),
-      //       ],
-      //     ),
-      //   ),
-      // ),
     );
   }
 }
