b50c34a904794e1559e7a7ba68308a2ceaee1d31
Kauê Martins <ksampaiosp@gmail.com>
2023-09-04 16:26:30 -0300

    test: add new test

--- packages/flutter/flutter_context_dtx/test/flutter_context_dtx_test.dart
+++ packages/flutter/flutter_context_dtx/test/flutter_context_dtx_test.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  group('flutter_context_dtx', () {});
+  group('flutter_context_dtx', () {
+    test('flutter_context_dtx', () {});
+  });
 }
