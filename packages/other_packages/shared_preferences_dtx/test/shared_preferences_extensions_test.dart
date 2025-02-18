efc84bc1400d7236df1fad71a9480761604664d6
Kauê Martins <ksampaiosp@gmail.com>
2025-02-18 08:46:12 -0300

    test: return shared preferences ext test

--- packages/other_packages/shared_preferences_dtx/test/shared_preferences_extensions_test.dart
+++ packages/other_packages/shared_preferences_dtx/test/shared_preferences_extensions_test.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:shared_preferences_dtx/shared_preferences_dtx.dart';
-import 'package:shared_preferences_platform_interface/in_memory_shared_preferences_async.dart';
-import 'package:shared_preferences_platform_interface/shared_preferences_async_platform_interface.dart';
 
 class User {
   final String name;
@@ -32,66 +30,60 @@ class User {
 }
 
 void main() {
-  group('SharedPreferencesAsyncAsyncExtensions', () {
-    late SharedPreferencesAsync preferences;
+  group('SharedPreferencesExtensions', () {
+    late SharedPreferences preferences;
     const user = User('user', 20);
     final dateTime = DateTime.utc(2022, 07, 12, 03, 04, 05, 99);
     final differentDateTime = DateTime.utc(2021, 01, 26, 03, 04, 05, 99);
 
     setUp(() async {
-      SharedPreferencesAsyncPlatform.instance =
-          InMemorySharedPreferencesAsync.empty();
-      preferences = SharedPreferencesAsync();
+      SharedPreferences.setMockInitialValues({});
+      preferences = await SharedPreferences.getInstance();
     });
 
     tearDown(() => preferences.clear());
 
-    test('reading default value', () async {
-      final (
-        stringValue,
-        boolValue,
-        intValue,
-        doubleValue,
-        listStringValue,
-        colorValue,
-        themeModeValue,
-        dateTimeValue,
-        userJson
-      ) = await (
+    test('reading default value', () {
+      expect(
         preferences.getOrElse<String>('string', defaultValue: 'String'),
-        preferences.getOrElse<bool>('bool', defaultValue: false),
-        preferences.getOrElse<int>('int', defaultValue: 2),
-        preferences.getOrElse<double>('double', defaultValue: 2.5),
+        'String',
+      );
+      expect(preferences.getOrElse<bool>('bool', defaultValue: false), false);
+      expect(preferences.getOrElse<int>('int', defaultValue: 2), 2);
+      expect(preferences.getOrElse<double>('double', defaultValue: 2.5), 2.5);
+      expect(
         preferences.getOrElse<List<String>>(
           'list',
           defaultValue: <String>['foo', 'bar'],
         ),
+        <String>['foo', 'bar'],
+      );
+      expect(
         preferences.getOrElse<Color>('color', defaultValue: Colors.black),
+        Colors.black,
+      );
+      expect(
         preferences.getOrElse<ThemeMode>(
           'theme_mode',
           defaultValue: ThemeMode.dark,
         ),
+        ThemeMode.dark,
+      );
+      expect(
         preferences.getOrElse<DateTime>('date_time', defaultValue: dateTime),
-        preferences.getOrElse<Map<String, dynamic>>(
-          'user',
-          defaultValue: user.toJson(),
-        ),
-      ).wait;
-      expect(stringValue, 'String');
-      expect(boolValue, false);
-      expect(intValue, 2);
-      expect(doubleValue, 2.5);
-      expect(listStringValue, <String>['foo', 'bar']);
-      expect(colorValue, Colors.black);
-      expect(themeModeValue, ThemeMode.dark);
-      expect(dateTimeValue, dateTime);
+        dateTime,
+      );
+      final userJson = preferences.getOrElse<Map<String, dynamic>>(
+        'user',
+        defaultValue: user.toJson(),
+      );
       expect(userJson, user.toJson());
       expect(User.fromJson(userJson), user);
     });
 
     test('writing and reading', () async {
       await Future.wait([
-        //preferences.setValue<String>('string', 'String'),
+        preferences.setValue<String>('string', 'String'),
         preferences.setValue<bool>('bool', false),
         preferences.setValue<int>('int', 2),
         preferences.setValue<double>('double', 2.5),
@@ -102,36 +94,18 @@ void main() {
         preferences.setValue<DateTime>('date_time', dateTime),
       ]);
 
-      final (
-        stringValue,
-        boolValue,
-        intValue,
-        doubleValue,
-        listStringValue,
-        colorValue,
-        themeModeValue,
-        dateTimeValue,
-        userJson
-      ) = await (
-        preferences.getValue<String>('string'),
-        preferences.getValue<bool>('bool'),
-        preferences.getValue<int>('int'),
-        preferences.getValue<double>('double'),
+      expect(preferences.getValue<String>('string'), 'String');
+      expect(preferences.getValue<bool>('bool'), isFalse);
+      expect(preferences.getValue<int>('int'), 2);
+      expect(preferences.getValue<double>('double'), 2.5);
+      expect(
         preferences.getValue<List<String>>('list'),
-        preferences.getValue<Color>('color'),
-        preferences.getValue<ThemeMode>('theme_mode'),
-        preferences.getValue<DateTime>('date_time'),
-        preferences.getValue<Map<String, dynamic>>('user'),
-      ).wait;
-
-      // expect(stringValue, 'String');
-      expect(boolValue, isFalse);
-      expect(intValue, 2);
-      expect(doubleValue, 2.5);
-      expect(listStringValue, <String>['foo', 'bar']);
-      expect(colorValue, Colors.black);
-      expect(themeModeValue, ThemeMode.system);
-      expect(dateTimeValue, dateTime);
+        <String>['foo', 'bar'],
+      );
+      expect(preferences.getValue<Color>('color'), Colors.black);
+      expect(preferences.getValue<ThemeMode>('theme_mode'), ThemeMode.system);
+      expect(preferences.getValue<DateTime>('date_time'), dateTime);
+      final userJson = preferences.getValue<Map<String, dynamic>>('user');
       expect(userJson, isNotNull);
       if (userJson != null) {
         expect(User.fromJson(userJson), user);
@@ -150,83 +124,66 @@ void main() {
         preferences.setValue('user', user.toJson()),
         preferences.setValue('date_time', dateTime),
       ]);
-      final (
-        stringValue,
-        boolValue,
-        intValue,
-        doubleValue,
-        listStringValue,
-        colorValue,
-        themeModeValue,
-        dateTimeValue,
-        userJson
-      ) = await (
-        preferences.getOrElse('string', defaultValue: 'test'),
-        preferences.getOrElse('bool', defaultValue: true),
-        preferences.getOrElse('int', defaultValue: 10),
-        preferences.getOrElse('double', defaultValue: 12.5),
+      expect(preferences.getOrElse('string', defaultValue: 'test'), 'String');
+      expect(preferences.getOrElse('bool', defaultValue: true), false);
+      expect(preferences.getOrElse('int', defaultValue: 10), 2);
+      expect(preferences.getOrElse('double', defaultValue: 12.5), 2.5);
+      expect(
         preferences.getOrElse('list', defaultValue: <String>['foo']),
+        <String>['foo', 'bar'],
+      );
+      expect(
         preferences.getOrElse('color', defaultValue: Colors.white),
+        Colors.black,
+      );
+      expect(
         preferences.getOrElse('theme_mode', defaultValue: ThemeMode.dark),
+        ThemeMode.light,
+      );
+      expect(
         preferences.getOrElse('date_time', defaultValue: differentDateTime),
-        preferences.getOrElse(
-          'user',
-          defaultValue: const User('other', 50).toJson(),
-        ),
-      ).wait;
-
-      expect(stringValue, 'String');
-      expect(boolValue, false);
-      expect(intValue, 2);
-      expect(doubleValue, 2.5);
-      expect(listStringValue, <String>['foo', 'bar']);
-      expect(colorValue, Colors.black);
-      expect(themeModeValue, ThemeMode.light);
-      expect(dateTimeValue, dateTime);
-      expect(userJson, user.toJson());
+        dateTime,
+      );
+      final userJson = preferences.getOrElse(
+        'user',
+        defaultValue: const User('other', 50).toJson(),
+      );
+      expect(User.fromJson(userJson), user);
     });
 
     group('getTypeOrDefault', () {
-      test('gets default', () async {
-        final (
-          stringValue,
-          boolValue,
-          intValue,
-          doubleValue,
-          listStringValue,
-          colorValue,
-          themeModeValue,
-          dateTimeValue,
-          userJson
-        ) = await (
-          preferences.getOrElse<String>('string', defaultValue: 'String'),
-          preferences.getOrElse<bool>('bool', defaultValue: false),
-          preferences.getOrElse<int>('int', defaultValue: 2),
-          preferences.getOrElse<double>('double', defaultValue: 2.5),
-          preferences.getOrElse<List<String>>(
+      test('gets default', () {
+        expect(
+          preferences.getStringOrElse('string', defaultValue: 'String'),
+          'String',
+        );
+        expect(preferences.getBoolOrElse('bool', defaultValue: false), false);
+        expect(preferences.getIntOrElse('int', defaultValue: 2), 2);
+        expect(preferences.getDoubleOrElse('double', defaultValue: 2.5), 2.5);
+        expect(
+          preferences.getStringListOrElse(
             'list',
             defaultValue: <String>['foo', 'bar'],
           ),
-          preferences.getOrElse<Color>('color', defaultValue: Colors.black),
-          preferences.getOrElse<ThemeMode>(
+          <String>['foo', 'bar'],
+        );
+        expect(
+          preferences.getColorOrElse('color', defaultValue: Colors.black),
+          Colors.black,
+        );
+        expect(
+          preferences.getThemeModeOrElse(
             'theme_mode',
             defaultValue: ThemeMode.dark,
           ),
-          preferences.getOrElse<DateTime>('date_time', defaultValue: dateTime),
-          preferences.getOrElse<Map<String, dynamic>>(
-            'user',
-            defaultValue: user.toJson(),
-          ),
-        ).wait;
-
-        expect(stringValue, 'String');
-        expect(boolValue, false);
-        expect(intValue, 2);
-        expect(doubleValue, 2.5);
-        expect(listStringValue, <String>['foo', 'bar']);
-        expect(colorValue, Colors.black);
-        expect(themeModeValue, ThemeMode.dark);
-        expect(dateTimeValue, dateTime);
+          ThemeMode.dark,
+        );
+        expect(
+          preferences.getDateTimeOrElse('date_time', defaultValue: dateTime),
+          dateTime,
+        );
+        final userJson =
+            preferences.getJsonOrElse('user', defaultValue: user.toJson());
         expect(userJson, user.toJson());
         expect(User.fromJson(userJson), user);
       });
@@ -243,47 +200,40 @@ void main() {
           preferences.setValue('user', user.toJson()),
           preferences.setValue('date_time', dateTime),
         ]);
-
-        final (
-          stringValue,
-          boolValue,
-          intValue,
-          doubleValue,
-          listStringValue,
-          colorValue,
-          themeModeValue,
-          dateTimeValue,
-          userJson
-        ) = await (
-          preferences.getOrElse<String>('string', defaultValue: 'test'),
-          preferences.getOrElse<bool>('bool', defaultValue: true),
-          preferences.getOrElse<int>('int', defaultValue: 10),
-          preferences.getOrElse<double>('double', defaultValue: 12.5),
-          preferences.getOrElse<List<String>>(
-            'list',
-            defaultValue: <String>['foo'],
-          ),
-          preferences.getOrElse<Color>('color', defaultValue: Colors.white),
-          preferences.getOrElse<ThemeMode>(
+        expect(
+          preferences.getStringOrElse('string', defaultValue: 'test'),
+          'String',
+        );
+        expect(preferences.getBoolOrElse('bool', defaultValue: true), false);
+        expect(preferences.getIntOrElse('int', defaultValue: 10), 2);
+        expect(preferences.getDoubleOrElse('double', defaultValue: 12.5), 2.5);
+        expect(
+          preferences
+              .getStringListOrElse('list', defaultValue: <String>['foo']),
+          <String>['foo', 'bar'],
+        );
+        expect(
+          preferences.getColorOrElse('color', defaultValue: Colors.white),
+          Colors.black,
+        );
+        expect(
+          preferences.getThemeModeOrElse(
             'theme_mode',
             defaultValue: ThemeMode.dark,
           ),
-          preferences.getOrElse<DateTime>('date_time', defaultValue: dateTime),
-          preferences.getOrElse<Map<String, dynamic>>(
-            'user',
-            defaultValue: user.toJson(),
+          ThemeMode.light,
+        );
+        expect(
+          preferences.getDateTimeOrElse(
+            'date_time',
+            defaultValue: differentDateTime,
           ),
-        ).wait;
-
-        expect(stringValue, 'String');
-        expect(boolValue, false);
-        expect(intValue, 2);
-        expect(doubleValue, 2.5);
-        expect(listStringValue, <String>['foo', 'bar']);
-        expect(colorValue, Colors.black);
-        expect(themeModeValue, ThemeMode.light);
-        expect(dateTimeValue, dateTime);
-        expect(userJson, user.toJson());
+          dateTime,
+        );
+        final userJson = preferences.getJsonOrElse(
+          'user',
+          defaultValue: const User('other', 50).toJson(),
+        );
         expect(User.fromJson(userJson), user);
       });
     });
@@ -294,28 +244,24 @@ void main() {
         throwsArgumentError,
       );
       expect(
-        () => preferences.getValue<SharedPreferencesAsync>('value'),
+        () => preferences.getValue<SharedPreferences>('value'),
         throwsArgumentError,
       );
       expect(
-        () => preferences.getOrElse<SharedPreferencesAsync>(
+        () => preferences.getOrElse<SharedPreferences>(
           'value',
           defaultValue: preferences,
         ),
         throwsArgumentError,
       );
       expect(
-        () => preferences.setValue<SharedPreferencesAsync>(
-          'preferences',
-          preferences,
-        ),
+        () =>
+            preferences.setValue<SharedPreferences>('preferences', preferences),
         throwsArgumentError,
       );
       expect(
-        () => preferences.setValue<SharedPreferencesAsync>(
-          'preferences',
-          preferences,
-        ),
+        () =>
+            preferences.setValue<SharedPreferences>('preferences', preferences),
         throwsArgumentError,
       );
     });
